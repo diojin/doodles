dd4d5fd83d059cbf8d0ae7073c06bc1414240223
dio jin <diojin@gmail.com>
2012-04-03 11:21:41 +0500

    CommonUtils back again, miss you so much

--- doodles_scripts/perl/lib/CommonUtils.pm
+++ doodles_scripts/perl/lib/CommonUtils.pm
@@ -1,3 +1,21 @@
+package CommonUtils;
 use strict;
 use warnings;
-use Switch;
+use Exporter;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(dos_command dos_command_no_output);
+%EXPORT_TAGS = ( DEFAULT => [qw(&dos_command)],
+                 All    => [qw(&dos_command &dos_command_no_output)]);
+                 
+sub dos_command{
+	my $result = `cmd //C $_[0]`;
+	$result =~ s/\s*$//g ;
+	return $result;	
+}
+sub dos_command_no_output{
+	return system("cmd //C $_[0]");	
+}
