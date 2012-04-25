1c4fc6bb3dbc8fb95bb99deaccc4cb340c07fd25
dio jin <diojin@gmail.com>
2012-04-25 08:08:48 +0500

    . cache xml file configed by spring gemfire integration . spring-gemfire.xsd added as a shortcut reference . new testcase for registering key interest

--- doodles_gemfire/src/main/java/outerhaven/gemfire/test/Test.java
+++ doodles_gemfire/src/main/java/outerhaven/gemfire/test/Test.java
@@ -1,5 +1,10 @@
 package outerhaven.gemfire.test;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import outerhaven.gemfire.GatewayHubControl;
 
 import com.gemstone.gemfire.cache.Region;
@@ -9,16 +14,28 @@ import com.gemstone.gemfire.cache.client.PoolManager;
 
 public class Test {
 	public static void main(String[] args){
+		registerInterestTest();
+	}
+	public static void registerInterestTest(){
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("gemfire/sgf-client-base.xml");
+		ctx.registerShutdownHook();
+		try {
+			new CountDownLatch(1).await();
+		}
+		catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}		
+	}
+	public static void gatewayTest() {
 		ClientCache cache = new ClientCacheFactory()
-			.set("name", "clientworker")
-			.set("cache-xml-file", "gemfire/client_base.xml")
-			.create();
-		
+				.set("name", "clientworker")
+				.set("cache-xml-file", "gemfire/client_base.xml").create();
+
 		Region<String, String> exampleRegion = cache.getRegion("exampleRegion");
 		exampleRegion.put("key0", "test0");
 		exampleRegion.put("key1", "test1");
 		GatewayHubControl ctl = new GatewayHubControl();
 		ctl.setPool(PoolManager.find("base_pool"));
-		ctl.showGatewayQueueSize();		
+		ctl.showGatewayQueueSize();
 	}
 }
